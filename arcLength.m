51157191fce290e7d623d9ff95f838e65f435465
Alex Santiago <davisitosantiago11@gmail.com>
2022-12-19 21:17:43 -0400

    Added arcLength.m and in2cm.m

---
+++ arcLength.m
@@ -0,0 +1,7 @@
+function s = arcLength(r, theta)
+% arcLength Calculates the arc length based on radius and angle
+%
+%r: Radius
+%theta: [radians] Angle
+    s = r * theta;
+end
---
+++ in2cm.m
@@ -0,0 +1,7 @@
+function cm = in2cm(in)
+    % in2cm converts inches to centimetes
+    %
+    %in: Input in inches
+    
+    cm = in * 2.54;
+end
