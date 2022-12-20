80f093fd7b9c07d515216dac772c1725880380c0
Alex Santiago <davisitosantiago11@gmail.com>
2022-12-19 20:53:25 -0400

    Updated pltAcademic.m documentation and parameters

--- pltAcademic.m
+++ pltAcademic.m
@@ -1,41 +1,53 @@
-function pltAcademic(fig__)
+function pltAcademic(fig__, varargin)
 %pltAcademic Figure Format for Academic Publications
-%   Detailed explanation goes here
-% fig__: Figure Handle
-% TODO: Documentation, input arguments call as options
-
-    show_title = 0; % Option to show title on top of Figure
-    box = 'off'; % Figure Bounding Box toggle
-    picturewidth = 8.89; % [cm] Figure Width
-    lw = 1.5; % Line Width
-    msz = 6; % Marker Size
-    hw_ratio = 0.65; % feel free to play with this ratio
-    font = 11;  % Axis and Labels Font Size
-    font_title = 11; % Title Font Size
-    legend_loc = 'best'; % Legend location
-
+%   Automate the formating of figures for academic publications or
+%   presentations.
+%
+%fig__: Figure Handle
+%title: Show title on Figure option
+%box: Figure Bounding Box option
+%width: [cm] Figure Width
+%lw: Line Width
+%msz: Marker Size
+%hwRatio: Height-Width Ratio
+%font: Font Size for Text
+%fontTitle: Font Size for Title
+%showLegend: Show Legend Option
+%legendLoc: Legend Location
+    
     p = inputParser;
-    addParameter(p,'show_title',0)
+    addParameter(p,'title',0);    % Option to show title on top of Figure
+    addParameter(p,'box','off');  % Figure Bounding Box toggle
+    addParameter(p,'width',8.89); % [cm] Figure Width
+    addParameter(p,'lw',1.5); % Line Width
+    addParameter(p,'msz',6); % Marker Size
+    addParameter(p,'hwRatio',0.65); % Height to Width Ratio
+    addParameter(p,'font',11);  % Axis and Labels Font Size  
+    addParameter(p,'fontTitle',11); % Title Font Size
+    addParameter(p,'showLegend',0); % Legend    
+    addParameter(p,'legendLoc','best'); % Legend location    
+    parse(p,varargin{:});
 
-
-    legend('Location',legend_loc)
-    set(findall(fig__,'-property','FontSize'),'FontSize',font) % adjust fontsize to your  document
-    if show_title
-        title(fig__.Name, 'FontSize', font_title)
+    if p.Results.showLegend
+        legend('Location',p.Results.legendLoc)
     end
-    set(findall(fig__,'-property','Box'),'Box', box) % optional
+    set(findall(fig__,'-property','FontSize'),'FontSize',p.Results.font) % adjust fontsize to your  document
+    if p.Results.title
+        title(fig__.Name, 'FontSize', p.Results.fontTitle)
+    end
+    set(findall(fig__,'-property','Box'),'Box', p.Results.box) % optional
     set(findall(fig__,'-property','Interpreter'),'Interpreter','latex')
     set(findall(fig__,'-property','TickLabelInterpreter'),'TickLabelInterpreter','latex')
-    set(findall(fig__,'-property','LineWidth'),'LineWidth',lw)
-    set(findall(fig__,'-property','MarkerSize'),'MarkerSize',msz)
-    set(fig__,'Units','centimeters','Position',[3 3 picturewidth hw_ratio*picturewidth])
+    set(findall(fig__,'-property','LineWidth'),'LineWidth',p.Results.lw)
+    set(findall(fig__,'-property','MarkerSize'),'MarkerSize',p.Results.msz)
+    set(fig__,'Units','centimeters','Position',[3 3 p.Results.width p.Results.hwRatio*p.Results.width])
     axis tight
     pos = get(fig__,'Position');
-    set(fig__,'PaperPositionMode','Auto','PaperUnits','centimeters','PaperSize',[pos(3), pos(4)])
+    set(fig__,'PaperPositionMode','Auto','PaperUnits','centimeters','PaperSize',[pos(3) + 0.1, pos(4) + 0.1])
     
     [~,~,~] = mkdir('img');
     d = datetime;
-    d.Format = 'yyyy-MM-dd HH:mm';
-    print(fig__,strcat('img/', string(d), "_", fig__.Name),'-dpdf','-vector','-fillpage')
-    print(fig__,strcat('img/', string(d), "_", fig__.Name),'-dpng','-vector')
+    d.Format = 'yyyy-MM-dd HHmm';
+    print(fig__, strcat('img/', string(d), "_", fig__.Name),'-dpdf','-vector');
+    print(fig__, strcat('img/', string(d), "_", fig__.Name),'-dpng','-vector', '-r300')
 end
